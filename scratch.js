a88de9e170fed7f8f7ce852975823db1e737e97a
한은이 <haneuneee@gmail.com>
2020-06-04 19:47:55 +0900

    Solved dsa

--- scratch.js
+++ scratch.js
@@ -1,3 +1,65 @@
-export function greeting(name) {
-  return `Hello, ${name}.`;
+export function getNearestSmallers(space, lefthand = false) {
+  const stack = [];
+  const defaultIndex = lefthand ? -1 : space.length;
+  const result = Array(space.length).fill(defaultIndex);
+
+  for (let i = 0; i < space.length; i++) {
+    const currentIndex = lefthand ? space.length - 1 - i : i;
+    while (
+      stack.length > 0 &&
+      stack[stack.length - 1].value > space[currentIndex]
+    ) {
+      result[stack.pop().index] = currentIndex;
+    }
+    stack.push({
+      index: currentIndex,
+      value: space[currentIndex],
+    });
+  }
+
+  return result;
 }
+export const dsa = [
+  // 0: N
+  (space, x) => {
+    const lns = getNearestSmallers(space, true);
+    const rns = getNearestSmallers(space);
+
+    let result;
+    for (let i = 0; i < space.length; i++) {
+      const mx = rns[i] - lns[i] - 1;
+      if (x <= mx) {
+        if (!result || result < space[i]) {
+          result = space[i];
+        }
+      }
+    }
+    return result;
+  },
+  // 1: (N-X)X
+  // (space, x) => {
+  //   let result;
+  //   for (let i = 0; i <= space.length - x; i++) {
+  //     let min = space[i];
+  //     for (let j = 0; j < x; j++) {
+  //       if (min > space[i + j]) {
+  //         min = space[i + j];
+  //       }
+  //     }
+  //     if (!result || result < min) {
+  //       result = min;
+  //     }
+  //   }
+  //   return result;
+  // },
+  // (space, x) => {
+  //   const segMin = [];
+  //   for (let i = 0; i < space.length; i++) {
+  //     const seg = space.slice(i, i + x);
+  //     const min = Math.min(...seg);
+  //     segMin.push(min);
+  //   }
+  //   const result = Math.max(...segMin);
+  //   return result;
+  // },
+];
--- scratch.test.js
+++ scratch.test.js
@@ -1,10 +1,97 @@
-import { greeting } from "./scratch";
+import { dsa, getNearestSmallers } from "./scratch";
+const maxSpace = Math.pow(10, 9);
+const getRandom = (end) => Math.floor(Math.random() * end + 1);
+const getRandomSpace = (length) =>
+  Array(length)
+    .fill(0)
+    .map(() => getRandom(maxSpace));
 
-test.each([
-  ["John", "Hello, John."],
-  ["Anna", "Hello, Anna."],
-])("greeting(%s) returns '%s'", (name, expected) => {
-  const result = greeting(name);
+const testCases = [
+  [[1, 2, 3, 1, 2], 1, 3],
+  [[1, 1, 1], 2, 1],
+  [[2, 5, 4, 6, 8], 3, 4],
+];
 
-  expect(result).toBe(expected);
+jest.setTimeout(100);
+
+describe.each([
+  // 0 < N <= 10^5, 0 < X <= N, 0 < space[i] <= 10^9
+  [Math.pow(10, 4), Math.pow(10, 0)],
+  [Math.pow(10, 4), Math.pow(10, 4)],
+  [Math.pow(10, 4), Math.pow(10, 4) / 2],
+
+  [Math.pow(10, 5), Math.pow(10, 0)],
+  [Math.pow(10, 5), Math.pow(10, 5)],
+  [Math.pow(10, 5), Math.pow(10, 5) / 2],
+
+  [Math.pow(10, 6), Math.pow(10, 0)],
+  [Math.pow(10, 6), Math.pow(10, 6)],
+  [Math.pow(10, 6), Math.pow(10, 6) / 2],
+])("random cases", (n, x) => {
+  let start;
+  const results = [];
+  const space = getRandomSpace(n);
+  test.each(dsa.map((_, i) => i))(
+    "all results are same",
+    (i) => {
+      start = new Date();
+      const result = dsa[i](space, x);
+      console.log(`${i}:[${n}:${x}] ${new Date() - start} ms`);
+      if (results.length > 0) {
+        expect(results.pop()).toStrictEqual(result);
+      }
+      results.push(result);
+    },
+    10
+  );
+});
+
+describe("main", () => {
+  dsa.forEach((_, i) => {
+    test.each(testCases)(`dsa[${i}] (%s, %s) -> %s`, (space, x, expected) => {
+      const result = dsa[i](space, x);
+
+      expect(result).toBe(expected);
+    });
+  });
+});
+
+describe("getNearestSmallers", () => {
+  test.each([
+    [
+      [1, 2, 3, 1, 2],
+      [5, 3, 3, 5, 5],
+    ],
+    [
+      [1, 1, 1],
+      [3, 3, 3],
+    ],
+    [
+      [2, 5, 4, 6, 8],
+      [5, 2, 5, 5, 5],
+    ],
+  ])("(%s) -> %s", (space, expected) => {
+    const result = getNearestSmallers(space);
+
+    expect(result).toStrictEqual(expected);
+  });
+
+  test.each([
+    [
+      [1, 2, 3, 1, 2],
+      [-1, 0, 1, -1, 3],
+    ],
+    [
+      [1, 1, 1],
+      [-1, -1, -1],
+    ],
+    [
+      [2, 5, 4, 6, 8],
+      [-1, 0, 0, 2, 3],
+    ],
+  ])("Reverse (%s) -> %s", (space, expected) => {
+    const result = getNearestSmallers(space, true);
+
+    expect(result).toStrictEqual(expected);
+  });
 });
